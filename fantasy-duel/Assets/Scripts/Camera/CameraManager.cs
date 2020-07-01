4db3572bce722798257181acf9ca00e6c684a90a
Fábio Lamin <fabio_lamin@hotmail.com>
2020-07-01 09:01:32 -0300

    Create Camera Manager script

---
+++ fantasy-duel/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private static CameraManager instance;
+    private bool HasKeyBeenPressed = false;
+
+    [SerializeField] private Transform camera;
+    [SerializeField] private Transform destination;
+    [SerializeField] private float speed = 1500f;
+
+    public static CameraManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("Camera Manager is NULL.");
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+            HasKeyBeenPressed = true;
+
+        if (HasKeyBeenPressed)
+            Move();
+    }
+
+    private void Move()
+    {
+        if (camera.transform.position != destination.position)
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, destination.position, speed * Time.deltaTime);
+    }
+}
