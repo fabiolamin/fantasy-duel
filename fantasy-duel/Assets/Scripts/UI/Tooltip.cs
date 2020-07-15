ae2269b17e4f3c0d30cb713e56901cb6ed910efe
Fábio Lamin <fabio_lamin@hotmail.com>
2020-07-15 11:13:20 -0300

    Implement tooltips at board

---
+++ fantasy-duel/Assets/Scripts/UI/Tooltip.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private PlayerManager playerManager;
+
+    private void Awake()
+    {
+        playerManager = transform.root.GetComponent<PlayerManager>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ActiveTooltip(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ActiveTooltip(false);
+    }
+
+    private void ActiveTooltip(bool isActivated)
+    {
+        if (playerManager.PhotonView.IsMine)
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.CompareTag("Tooltip"))
+                    child.gameObject.SetActive(isActivated);
+            }
+        }
+    }
+}
